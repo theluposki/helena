652ab2745fedb21874baaae2692d130c3f62347e
theluposki <theluposki@gmail.com>
2022-07-01 16:49:58 -0300

    conf

---
+++ dist/precache-manifest.22bd08077fcb882c595fd32727d6fd2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117465854c9e9ef9617f",
+    "url": "/css/app.cd7de242.css"
+  },
+  {
+    "revision": "490c3ffea9ea76bf76b888f29541569d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "117465854c9e9ef9617f",
+    "url": "/js/app.699b7c62.js"
+  },
+  {
+    "revision": "5c5a88acadd58056cc47",
+    "url": "/js/chunk-vendors.d3740ba4.js"
+  },
+  {
+    "revision": "2f88b88e34b36b53bf6357602c2f4f7a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
